99d559dc5574bac2a7d5495b856a86bdae5b1044
Ajay Singh Deopa <singhdeopa>
2016-06-19 01:48:40 +0530

    First commit

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use App\User;
+
+class DemoController extends Controller
+{
+    /**
+     * Retrieve the user for the given ID.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function demo()
+    {   return response()->json("hkjhkjhkjhjhj");
+        
+    }
+}
